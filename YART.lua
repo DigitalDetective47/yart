85a762a3a3152b86cd69020109dc67385a34dcc1
DigitalDetective47 <ninji2701@gmail.com>
2024-11-27 03:50:51 -0500

    add mod icon

---
+++ YART.lua
@@ -0,0 +1,13 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Yet Another Reversed Tarot mod
+--- MOD_ID: YART
+--- MOD_AUTHOR: [DigitalDetective47]
+--- MOD_DESCRIPTION: A Balatro mod that adds reversed tarot cards to the game
+--- PREFIX: yart
+
+SMODS.Atlas({
+    key = "modicon",
+    path = "icon.png",
+    px = 34,
+    py = 34,
+})
