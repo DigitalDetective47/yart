024c6c1caca8471a74ebcfc262c89e1eb1d97ff4
DigitalDetective47 <ninji2701@gmail.com>
2024-11-27 05:20:40 -0500

    add names for all cards

---
+++ localization/en-us.lua
@@ -0,0 +1,116 @@
+return {
+    descriptions = {
+        Tarot = {
+            c_yart_rfool = {
+                name = "looF ehT",
+                text = {
+                }
+            },
+            c_yart_rmagician = {
+                name = "naicigaM ehT",
+                text = {
+                }
+            },
+            c_yart_rhigh_priestess = {
+                name = "ssetseirP hgiH ehT",
+                text = {
+                }
+            },
+            c_yart_rempress = {
+                name = "sserpmE ehT",
+                text = {
+                }
+            },
+            c_yart_remperor = {
+                name = "rorepmE ehT",
+                text = {
+                }
+            },
+            c_yart_rheirophant = {
+                name = "tnahporeiH ehT",
+                text = {
+                }
+            },
+            c_yart_rlovers = {
+                name = "srevoL ehT",
+                text = {
+                }
+            },
+            c_yart_rchariot = {
+                name = "toirahC ehT",
+                text = {
+                }
+            },
+            c_yart_rjustice = {
+                name = "ecitsuJ",
+                text = {
+                }
+            },
+            c_yart_rhermit = {
+                name = "timreH ehT",
+                text = {
+                }
+            },
+            c_yart_rwheel_of_fortune = {
+                name = "enutroF fo leehW ehT",
+                text = {
+                }
+            },
+            c_yart_rstrength = {
+                name = "htgnertS",
+                text = {
+                }
+            },
+            c_yart_rhanged_man = {
+                name = "naM degnaH ehT",
+                text = {
+                }
+            },
+            c_yart_rdeath = {
+                name = "htaeD",
+                text = {
+                }
+            },
+            c_yart_rtemperance = {
+                name = "ecnarepmeT",
+                text = {
+                }
+            },
+            c_yart_rdevil = {
+                name = "liveD ehT",
+                text = {
+                }
+            },
+            c_yart_rtower = {
+                name = "rewoT ehT",
+                text = {
+                }
+            },
+            c_yart_rstar = {
+                name = "ratS ehT",
+                text = {
+                }
+            },
+            c_yart_rmoon = {
+                name = "nooM ehT",
+                text = {
+                }
+            },
+            c_yart_rsun = {
+                name = "nuS ehT",
+                text = {
+                }
+            },
+            c_yart_rjudgement = {
+                name = "tnemegduJ",
+                text = {
+                }
+            },
+            c_yart_rworld = {
+                name = "dlroW ehT",
+                text = {
+                }
+            },
+        }
+    }
+}
